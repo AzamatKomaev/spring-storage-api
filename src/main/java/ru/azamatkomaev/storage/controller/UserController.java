124f9cb7b25e94f0f4753cbbfe24784e117b828a
Азамат Комаев <azamatkomaev15@gmail.com>
2023-03-29 16:22:51 +0300

    Delete useless imports in files

--- src/main/java/ru/azamatkomaev/storage/controller/UserController.java
+++ src/main/java/ru/azamatkomaev/storage/controller/UserController.java
@@ -1,17 +1,14 @@
 package ru.azamatkomaev.storage.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.azamatkomaev.storage.model.User;
 import ru.azamatkomaev.storage.response.UserResponse;
 import ru.azamatkomaev.storage.service.UserService;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/api/v1/users")
